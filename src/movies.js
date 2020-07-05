9176ad1fc81a4c96c2da65ff6b96b12d277fba2a
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-07-05 02:01:35 +0100

    Completed all iterations

--- src/movies.js
+++ src/movies.js
@@ -22,25 +22,118 @@ function howManyMovies(array) {
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
 function ratesAverage(array) {
-  return (
-    Math.round(
-      (array
-        .map(element => element.rate)
-        .reduce((sum, value) => {
-          return sum + value;
-        }, 0) /
-        array.length) *
-        100
-    ) / 100
-  );
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return (
+      Math.round(
+        (array
+          .map(element => element.rate)
+          .reduce((sum, value) => {
+            return value ? sum + value : sum;
+          }, 0) /
+          array.length) *
+          100
+      ) / 100
+    );
+  }
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(array) {
+  const dramaMovies = array.filter(element =>
+    element.genre.some(genre => genre === 'Drama')
+  );
+  return ratesAverage(dramaMovies);
+}
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+function orderByYear(array) {
+  return array.slice().sort((a, b) => {
+    if (a.year === b.year) {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+    }
+    return a.year - b.year;
+  });
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically(array) {
+  const sortedTitles = array.slice().sort((a, b) => {
+    return a.title.localeCompare(b.title);
+  });
+  const arrayOfTitles = sortedTitles.map(element => element.title);
+  const limitedTo20 = arrayOfTitles.slice(0, 20);
+  return limitedTo20;
+}
+
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
+function turnHoursToMinutes(array) {
+  return array.map(element => {
+    let newElement = { ...element };
+    const timeArray = newElement.duration.split(' ');
+    if (timeArray.length > 1) {
+      const timesAndMinutes = timeArray.map(element => {
+        return parseInt(element.replace(/[^0-9]/g, ''));
+      });
+      const hour = timesAndMinutes[0];
+      const minutes = timesAndMinutes[1];
+      newElement.duration = hour * 60 + minutes;
+    } else {
+      const time = timeArray[0];
+      if (time.includes('h')) {
+        const hour = parseInt(time.replace(/[^0-9]/g, ''));
+        newElement.duration = hour * 60;
+      } else if (time.includes('min')) {
+        const minutes = parseInt(time.replace(/[^0-9]/g, ''));
+        newElement.duration = minutes;
+      }
+    }
+    return newElement;
+  });
+}
+
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+
+function bestYearAvg(array) {
+  if (array.length === 0) {
+    return null;
+  }
+  const years = [];
+  array.forEach(movie => {
+    if (!years.some(e => e.year === movie.year)) {
+      const yearObject = {
+        year: movie.year,
+        ratings: [movie.rate]
+      };
+      years.push(yearObject);
+    } else {
+      const yearObject = years.find(yearObj => {
+        return yearObj.year === movie.year;
+      });
+      yearObject.ratings.push(movie.rate);
+    }
+  });
+  years.forEach(year => {
+    const yearAverage =
+      Math.round(
+        (year.ratings.reduce((average, rating) => {
+          return average + rating;
+        }, 0) /
+          year.ratings.length) *
+          100
+      ) / 100;
+    year.average = yearAverage;
+  });
+  const bestYear = years.reduce((prev, current) => {
+    return prev.average > current.average ? prev : current;
+  });
+  return `The best year was ${bestYear.year} with an average rate of ${bestYear.average}`;
+
+  // Have not checked solution: note to self to try a different method without forEach
+}
